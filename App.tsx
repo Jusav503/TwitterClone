b9d69217a152f094b82786218432588aeaa4b70c
Justice Velasco <justicevgomez001@gmail.com>
2021-09-22 21:58:27 +0200

    AWS amplify implemented

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Amplify from 'aws-amplify'
+
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import config from './src/aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
