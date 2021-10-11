135a5d63f62d1a322f712ebea3fd28b3690aa62b
Justice Velasco <justicevgomez001@gmail.com>
2021-10-11 13:08:36 +0200

    submit and remove likes button enabled

--- components/Tweet/MainContainer/Footer/index.tsx
+++ components/Tweet/MainContainer/Footer/index.tsx
@@ -5,17 +5,14 @@ import { API, graphqlOperation, Auth } from "aws-amplify";
 
 import { TweetType } from "../../../../types";
 import styles from "./styles";
-import { createLike } from "../../../../graphql/mutations";
+import { createLike, deleteLike } from "../../../../graphql/mutations";
 
-export type FooterContainerProps = {
-  tweet: TweetType;
-};
+export type FooterContainerProps = {tweet: TweetType};
 const Footer = ({ tweet }: FooterContainerProps) => {
   const [myLike, setMyLike] = useState(null);
   const [user, setUser] = useState(null);
-
-  console.log(tweet);
-
+  const [likesCount, setLikesCount] = useState(tweet.likes.items.length);
+console.log(tweet);
   useEffect(() => {
     const fetchUser = async () => {
       const currentUser = await Auth.currentAuthenticatedUser();
@@ -28,11 +25,9 @@ const Footer = ({ tweet }: FooterContainerProps) => {
     };
     fetchUser();
   }, []);
-  const onLike = async () => {
-    if (!user) {
-      return;
-    }
 
+
+  const submitLike = async () => {
     const like = {
       userID: user.attributes.sub,
       tweetID: tweet.id,
@@ -41,13 +36,34 @@ const Footer = ({ tweet }: FooterContainerProps) => {
       const res = await API.graphql(
         graphqlOperation(createLike, { input: like })
       );
-
       setMyLike(res.data.createLike);
+      setLikesCount(likesCount + 1);
     } catch (e) {
       console.log(e);
     }
-  };
+  }
+  const removeLike = async () => {
+    try {
+      await API.graphql(graphqlOperation(deleteLike,{ 
+        input: { id: myLike.id }
+      }))
+      setLikesCount(likesCount - 1);
+      setMyLike(null);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
+  const onLike = async () => {
+    if (!user) {
+      return;
+    }
+    if(!myLike){
+      await submitLike()
+    }else{
+      await removeLike();
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
@@ -66,7 +82,7 @@ const Footer = ({ tweet }: FooterContainerProps) => {
             name={!myLike ? "hearto" : "heart"}
             color={!myLike ? "grey" : "red"}
           />
-          <Text>{tweet.numberOfLikes}</Text>
+          <Text>{likesCount}</Text>
         </View>
       </TouchableOpacity>
 
